ef567e152e594fc47f389dbde6e63365c38ca927
shiwei <379171631@qq.com>
2019-12-13 11:32:07 +0800

    m

--- lottery_data.js
+++ lottery_data.js
@@ -1,111 +1,528 @@
-var lottery_initial_datas =[
+var lottery_initial_datas = [
     {
         "nameen": "avatar1",
-        "namezh": "蒋勇",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "季萍",
+        "wish": "人事行政"
+    },
+    {
         "nameen": "avatar2",
-        "namezh": "于桂英",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "路秀换",
+        "wish": "人事行政"
+    },
+    {
         "nameen": "avatar3",
-        "namezh": "徐超",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "汪薇",
+        "wish": "财务部"
+    },
+    {
         "nameen": "avatar4",
-        "namezh": "孙秀兰",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "钱凌云",
+        "wish": "财务部"
+    },
+    {
         "nameen": "avatar5",
-        "namezh": "张娟",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "陈晨",
+        "wish": "财务部"
+    },
+    {
         "nameen": "avatar6",
-        "namezh": "潘秀兰",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "毕郑强",
+        "wish": "企划部"
+    },
+    {
         "nameen": "avatar7",
-        "namezh": "任伟",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "周朋许",
+        "wish": "企划部"
+    },
+    {
         "nameen": "avatar8",
-        "namezh": "何刚",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "叶先钱",
+        "wish": "企划部"
+    },
+    {
         "nameen": "avatar9",
-        "namezh": "万艳",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "刘扬",
+        "wish": "企划部"
+    },
+    {
         "nameen": "avatar10",
-        "namezh": "田秀英",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "史伟",
+        "wish": "企划部"
+    },
+    {
         "nameen": "avatar11",
-        "namezh": "崔伟",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "郭月",
+        "wish": "企划部"
+    },
+    {
         "nameen": "avatar12",
-        "namezh": "朱秀兰",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "何叶",
+        "wish": "采购部"
+    },
+    {
         "nameen": "avatar13",
-        "namezh": "金静",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "党圆圆",
+        "wish": "采购部"
+    },
+    {
         "nameen": "avatar14",
-        "namezh": "韩伟",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "李晖霞",
+        "wish": "采购部"
+    },
+    {
         "nameen": "avatar15",
-        "namezh": "邹勇",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "黄钰莹",
+        "wish": "采购部"
+    },
+    {
         "nameen": "avatar16",
-        "namezh": "许强",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "唐清清",
+        "wish": "采购部"
+    },
+    {
         "nameen": "avatar17",
-        "namezh": "赵杰",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "王婉婷",
+        "wish": "采购部"
+    },
+    {
         "nameen": "avatar18",
-        "namezh": "孟明",
-        "wish": "运营一部"
-        },
-        {
+        "namezh": "许和平",
+        "wish": "供应链"
+    },
+    {
         "nameen": "avatar19",
-        "namezh": "杨平",
+        "namezh": "王伟",
         "wish": "运营一部"
-        },
-        {
+    },
+    {
         "nameen": "avatar20",
-        "namezh": "史敏",
+        "namezh": "程静",
         "wish": "运营一部"
-        }
+    },
+    {
+        "nameen": "avatar21",
+        "namezh": "胡双",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar22",
+        "namezh": "谢玲",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar23",
+        "namezh": "涂玉婷",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar24",
+        "namezh": "尹玉德",
+        "wish": "运营一部"
+    },{
+        "nameen": "avatar25",
+        "namezh": "朱亚彤",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar26",
+        "namezh": "王震海",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar27",
+        "namezh": "廖露露",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar28",
+        "namezh": "常金彩",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar29",
+        "namezh": "胡小红",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar30",
+        "namezh": "邓芳",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar31",
+        "namezh": "易丽",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar32",
+        "namezh": "赵磊",
+        "wish": "运营一部"
+    },
+    {
+        "nameen": "avatar33",
+        "namezh": "朱漫",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar34",
+        "namezh": "韩珍",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar35",
+        "namezh": "朱晶晶",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar36",
+        "namezh": "柯冬雨",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar37",
+        "namezh": "许如莹",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar38",
+        "namezh": "彭单妹",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar39",
+        "namezh": "黄月月",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar40",
+        "namezh": "夏亚娟",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar41",
+        "namezh": "胡方芹",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar42",
+        "namezh": "沈小玲",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar43",
+        "namezh": "路雪",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar44",
+        "namezh": "高丽红",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar45",
+        "namezh": "乔三艳",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar46",
+        "namezh": "赵琳",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar47",
+        "namezh": "丰洁",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar48",
+        "namezh": "曲航",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar49",
+        "namezh": "贾青青",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar50",
+        "namezh": "瓮姚美",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar51",
+        "namezh": "梅艳",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar52",
+        "namezh": "陈凡",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar53",
+        "namezh": "刘珊珊",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar54",
+        "namezh": "邹雅丽",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar55",
+        "namezh": "冯银豆",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar56",
+        "namezh": "陈蒙洁",
+        "wish": "运营二部"
+    },
+    {
+        "nameen": "avatar57",
+        "namezh": "尚显贝",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar58",
+        "namezh": "王贤慎",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar59",
+        "namezh": "罗小嫚",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar60",
+        "namezh": "蒋培培",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar61",
+        "namezh": "陈俭军",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar62",
+        "namezh": "秦程鹏",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar63",
+        "namezh": "杨蒙",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar64",
+        "namezh": "巫彦君",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar65",
+        "namezh": "柯慧",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar66",
+        "namezh": "查道昆",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar67",
+        "namezh": "王咏",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar68",
+        "namezh": "徐含",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar69",
+        "namezh": "姜幼林",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar70",
+        "namezh": "王漫漫",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar71",
+        "namezh": "杨晶媛",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar72",
+        "namezh": "杨笑天",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar73",
+        "namezh": "王晓芬",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar74",
+        "namezh": "刘红燕",
+        "wish": "运营三部"
+    },
+    {
+        "nameen": "avatar75",
+        "namezh": "宋文杰",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar76",
+        "namezh": "谌方",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar77",
+        "namezh": "杨朝伟",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar78",
+        "namezh": "路荣",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar79",
+        "namezh": "曾玲芳",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar80",
+        "namezh": "崔明宽",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar81",
+        "namezh": "崔改革",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar82",
+        "namezh": "方蓓",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar83",
+        "namezh": "邹莲馨",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar84",
+        "namezh": "张娜娜",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar85",
+        "namezh": "刘格",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar86",
+        "namezh": "李永恒",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar87",
+        "namezh": "王瑶",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar88",
+        "namezh": "陈吕",
+        "wish": "运营五部"
+    },
+    {
+        "nameen": "avatar89",
+        "namezh": "刘世丽",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar90",
+        "namezh": "张碧君",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar91",
+        "namezh": "伍海灵",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar92",
+        "namezh": "李利霞",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar93",
+        "namezh": "郑雅静",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar94",
+        "namezh": "林秀云",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar95",
+        "namezh": "胡宁",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar96",
+        "namezh": "占小方",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar97",
+        "namezh": "郑玉婷",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar98",
+        "namezh": "王丽",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar99",
+        "namezh": "辜星燕",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar100",
+        "namezh": "耿丽",
+        "wish": "运营六部"
+    },
+    {
+        "nameen": "avatar101",
+        "namezh": "徐胜东",
+        "wish": "运营七部"
+    },
+    {
+        "nameen": "avatar102",
+        "namezh": "刘爽",
+        "wish": "运营七部"
+    },
+    {
+        "nameen": "avatar103",
+        "namezh": "陈梅",
+        "wish": "运营七部"
+    }
 ];
 
 var award_config = {
     "award01": 1,
-    "award02": 3,
-    "award03": 6,
-    "award04": 20
+    "award02": 2,
+    "award03": 3,
+    "award04": 6,
+    "award05": 10,
+    "award06": 20,
+    "award07": 10
 };
 
 // 初始化数据
